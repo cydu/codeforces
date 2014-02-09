489f2511a4de5996c279f357f10dc8bb2bec28f2
cydu <root@cydu.net>
2014-02-09 21:57:04 +0800

    Time limit exceeded on test 9: http://codeforces.com/contest/385/submission/5928017

--- 385C.cpp
+++ 385C.cpp
@@ -43,48 +43,6 @@ void prime_solve(vector<int> vec, const vector<int> &primes,
     }
 }
 
-class SegmentTree {
-public:
-    explicit SegmentTree(int n) {
-        _m = get_m(n);
-        _vec.clear();
-        _vec.resize(_m * 2 + 4);
-    }
-
-    void add(int key, int value) {
-        if (value == 0) {
-            return;
-        }
-        for (key += _m; key > 0; key >>= 1) {
-            _vec[key] += value;
-        }
-    }
-
-    int query(int l, int r) {
-        int ans = 0;
-        for (l += _m - 1, r += _m; l >> 1 != r >> 1; l >>= 1, r >>= 1) {
-            if (l % 2 == 0) {
-                ans += _vec[l + 1];
-            }
-            if (r % 2 == 1) {
-                ans += _vec[r - 1];
-            }
-        }
-        return ans;
-    }
-
-private:
-    int get_m(int n) {
-        int m = 8;
-        while (m < n) {
-            m *= 2;
-        }
-        return --m;
-    }
-    vector<int> _vec;
-    int _m;
-};
-
 int main(int argc, char* argv[]) {
     int n, m, l, r, mMax;
     cin >> n;
@@ -97,17 +55,18 @@ int main(int argc, char* argv[]) {
     vector<int> primes = prime_list(mMax + 3);
     vector<int> primes_nums(primes.size());
     prime_solve(vec, primes, primes_nums);
-    SegmentTree tree(primes.size() + 2);
+    vector<int> pre_sum(primes.size() + 1);
     for (int i = 0; i < primes.size(); ++i) {
-        tree.add(i + 1, primes_nums[i]);
+        pre_sum[i + 1] = pre_sum[i] + primes_nums[i];
     }
     cin >> m;
     for (int i = 0; i < m; ++i) {
         cin >> l >> r;
         vector<int>::iterator lit = lower_bound(primes.begin(), primes.end(), l);
         vector<int>::iterator rit = upper_bound(primes.begin(), primes.end(), r);
-        int sum = tree.query(distance(primes.begin(), lit) + 1, distance(primes.begin(), rit) + 1);
-        cout << sum << endl;
+        int lindex = distance(primes.begin(), lit);
+        int rindex = distance(primes.begin(), rit);
+        cout << pre_sum[rindex] - pre_sum[lindex] << endl;
     }
     return 0;
 }
